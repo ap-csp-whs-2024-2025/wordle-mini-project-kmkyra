e5f97e340ee4aac0157f405ff70013f651a3912e
kmkyra <kennedykyra69@gmail.com>
2025-01-30 21:59:16 +0000

    uhhh

--- wordle.cpp
+++ wordle.cpp
@@ -12,21 +12,25 @@
 #include <vector>    // std::vector
 
 
-std::vector<int> createSecret() {
-   std::vector<int> secret;
-    for(int i = 0; i < 4; i = i+1)
+std::vector<int> createSecret() 
+{
+   std::vector<int> secret = {};
+    int counter = 0;
+    while (counter < 4)
     {
-        secret.push_back(std::rand());
+        int val = rand() % 10;
+        secret.push_back(val);
+        counter = counter + 1;
     }
-    return secret;    // replace this with your code
+    return secret;
 }
 
-std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> guess) {
+std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> user_guess) {
     int index = 0;
-    std::vector<std::string> hint;
-    while(x<=4)
+    std::vector<std::string> hint = {};
+    while(index < 4)
     {
-       if(guess[index] = secret[index]) 
+       if(user_guess[index] == secret[index]) 
        {
         hint.push_back("O");
        }
@@ -37,14 +41,26 @@ std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> guess
        index = index + 1;
     }
    
-    return {"X", "X", "X", "X"};    // replace this with your code
+    return hint;    // replace this with your code
 //loop 4 times: if item 1 of secret matches item 1 of guess, 
-//popback O, else popback x.... index +1
+//pushback O, else pushback x.... index +1
 }
 
-bool winGame(std::vector<int> secret, std::vector<int> guess) {
-    // Write this procedure here
-    return false;    // replace this with your code
+bool winGame(std::vector<int> secret, std::vector<int> user_guess) 
+{
+    int index = 0;
+    while(index < 4)
+    {
+        if(user_guess[index] == secret[index])
+        {
+            index = index + 1;    
+        }  
+        else
+        {
+            return false;    
+        }
+    }
+    return true;
 }
 
 int main()
@@ -52,9 +68,7 @@ int main()
     // Seeding the random number generator
     // Uncomment the code when you are finished testing your program
     // and want to have the program run for real
-    // srand(time(0));
-    int random_num = rand() % 10;
-    
+     srand(time(0));
     
     std::vector<int> secret_code = createSecret();
     std::vector<int> user_guess = {};
@@ -73,7 +87,7 @@ int main()
         {
             int input;
             std::cin >> input;
-            guess.push_back(input);    // can also do append(guess, input);
+            user_guess.push_back(input);    // can also do append(guess, input);
         }
 
         hint = getHint(secret_code, user_guess);
